3109e76f1934ebdb4ec57e434266c6147db3a2f4
Biswadeep patra <42343581+BiswadeepPatra@users.noreply.github.com>
2020-09-13 00:21:11 +0530

    find max element in an array

---
+++ findMaxInArray.py
@@ -0,0 +1,13 @@
+# Initialize array
+arr = [25, 11, 7, 75, 56];
+
+# Initialize max with first element of array.
+max = arr[0];
+
+# Loop through the array
+for i in range(0, len(arr)):
+    # Compare elements of array with max
+    if (arr[i] > max):
+        max = arr[i];
+
+print("Largest element present in given array: " + str(max));
